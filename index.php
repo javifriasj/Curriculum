c8e70bbb20f6a0a04ce8014ee9523614c27bd76f
javifriasj <javifriasj@gmail.com>
2017-04-10 16:18:27 +0100

    1

--- index.php
+++ index.php
@@ -1,5 +1,130 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-	print "prueba";
+<head>
 
-?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Javier Frías jiménez</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <link href="Estilos/css.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top" class="index">
+
+<header>
+    <h3>Javier Frías Jiménez</h3>
+    <nav>
+        <ul>
+            <li class="active"><a href="">Sobre mí</a></li>
+            <li><a href="">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+<div class="textoMain">
+    <div class="textoMain1 textoMainAll">
+        <h3>Desarrollador de aplicaciones web</h3>
+        <p>
+            Tengo 21 años, soy técnico superior en desarrollo de aplicaciones web y administrador de Sistemas Operativos.
+        </p>
+        <p>
+            El diseño y el desarrollo web no es sólo mi carrera, sino también mi hobby desde que empecé en la informática.
+        </p>
+        <p>
+            Me gusta todo lo relacionado con la tecnología. sobretodo el mundo de la programación.
+        </p>
+    </div>
+
+     <div class="textoMain2 textoMainAll">
+        <h3>Conocimientos</h3>
+        <p>
+            Lenguajes del lado del cliente.
+        </p>
+        <ul>
+            <li>JavaScript</li>
+            <li>HTML5</li>
+        </ul>
+        <p>
+            Lenguajes del lado del Servidor.
+        </p>
+        <ul>
+            <li>Java</li>
+            <li>Visual C# .NET</li>
+            <li>NodeJs (básico)</li>
+            <li>PHP</li>
+        </ul>
+        <p>
+            Frameworks.
+        </p>
+        <ul>
+            <li>Bootstrap</li>
+            <li>Materialize</li>
+            <li>Symfony2</li>
+        </ul>
+    </div>
+</div>
+    <div class="imgMain">
+        <img class="img-circle" src="Images/foto.PNG">
+        <ul style="width: 0px">
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</main>
+
+<footer>
+    <div class="izqFooter">
+        <p>
+            Contacto  <span class="glyphicon glyphicon-envelope" aria-hidden="true" style="width: 30px"></span>
+            <a href="mailto:javierfriasjimenez@gmail.com">javierfriasjimenez@gmail.com</a>
+        </p>
+        
+    </div>
+    <div class="derFooter">
+        <nav>
+            <ul>
+                <li><img src="Images/twitter.png" width="30px" /><a href="https://twitter.com/jaavifriasj">Twitter</a></li>
+                <li><img src="Images/giy.png" width="30px" /><a href="https://github.com/javifriasj">GitHub</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/freelancer.min.js"></script>
+
+</body>
+
+</html>
